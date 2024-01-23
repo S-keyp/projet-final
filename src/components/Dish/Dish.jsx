a9bbb0590ecce47c886e3eed61f0c40b98639dbf
S-keyp <vicodin@orange.fr>
2024-01-23 23:15:29 +0100

    feat: menu and dish page refreshed

--- src/components/Dish/Dish.jsx
+++ src/components/Dish/Dish.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 export default function Dish({ dish }) {
 	const { addToCart } = useContext(CartContext)
-	
+
 	return (
 		<div className="dish-card">
 			<img className="dish-card__thumbnail" src={dish.image} />
@@ -16,10 +16,12 @@ export default function Dish({ dish }) {
 				<Link to={`/dish/${dish.id}`}>{dish.name}</Link>
 			</h5>
 
-			<h5>Ingredients:</h5>
-			<ul className='dish-card__list'>
-				{dish.ingredients.map((ingredient, index) => <li key={index}>{ingredient}</li>)}
-			</ul>
+			<div className="dish-card__body">
+				<h5>Ingredients:</h5>
+				<ul className='dish-card__list'>
+					{dish.ingredients.map((ingredient, index) => <li key={index}>{ingredient.replace(',', ' ~')}</li>)}
+				</ul>
+			</div>
 
 			<button onClick={() => addToCart(dish)} className='dish-card__action'>Add +</button>
 		</div>
