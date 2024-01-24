823f9d91bb47e03de6ea78222ffe04f21f96a831
S-Keyp <s-keyp@outlook.fr>
2024-01-24 21:10:17 +0100

    fix: link were not visible on sitemap

--- src/pages/Sitemap.jsx
+++ src/pages/Sitemap.jsx
@@ -2,25 +2,31 @@ import { Link } from "react-router-dom"
 
 export default function Sitemap() {
     return (
-        <ul>
-            <li>
-                <Link to="/presentation">Presentation</Link>
-            </li>
-            <li>
-                <Link to="/menu">Menu</Link>
-            </li>
-            <li>
-                <Link to="/location">Location</Link>
-            </li>
-            <li>
-                <Link to="/cart">Cart 🛒</Link>
-            </li>
-            <li>
-                <Link to="/order-validation">Order validation</Link>
-            </li>
-            <li>
-                <Link to="/not-found">Order validation</Link>
-            </li>
-        </ul>
+        <section className="section">
+            <h3 className="section__subtitle">Sitemap</h3>
+            <ul className="section__paragraph">
+                <li>
+                    <Link to="/presentation">Presentation</Link>
+                </li>
+                <li>
+                    <Link to="/menu">Menu</Link>
+                </li>
+                <li>
+                    <Link to="/dish/1">Dish detail example</Link>
+                </li>
+                <li>
+                    <Link to="/location">Location</Link>
+                </li>
+                <li>
+                    <Link to="/cart">Cart 🛒</Link>
+                </li>
+                <li>
+                    <Link to="/order-validation">Order validation</Link>
+                </li>
+                <li>
+                    <Link to="/not-found">Not found</Link>
+                </li>
+            </ul>
+        </section>
     )
 }
